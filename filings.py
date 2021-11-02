f723c2a9319493f05a12e0d9875f575235d2eb9c
Liza Daly <lizadaly@gmail.com>
2021-11-02 15:59:11 -0400

    Delete some unnecessary reports

--- filings.py
+++ filings.py
@@ -1,11 +1,9 @@
 from pathlib import Path
 import re
-from lxml import etree
 
 ticker_symbol = "TSLA"
 default_dir = "sec-edgar-filings"
-output_dir = "."
-output_file = "source.txt"
+output_file = "source.html"
 
 
 def download_if_needed(dir: Path):
@@ -14,30 +12,22 @@ def download_if_needed(dir: Path):
         from sec_edgar_downloader import Downloader
 
         dl = Downloader()
-        dl.get("10-Q", ticker_symbol)
+        dl.get("10-Q", ticker_symbol, after="2019-01-01")
 
 
 def clean_sgml_junk(dir: Path) -> list[str]:
     """Clean up an EDGAR filing by brute-force extracting all the text nodes of a reasonable length"""
     output: list[str] = []
-    min_node_length = 5
 
-    for filing_dir in dir.rglob("*"):
+    for filing_dir in sorted(dir.rglob("*")):
 
         filing = filing_dir / "full-submission.txt"
         if filing.exists():
             report = filing.open().read().replace("\n", "")
-            p = r"<XBRL>(.*?)</XBRL>"
+            p = r"<html>(.*?)</html>"
             if blob := re.search(p, report, re.MULTILINE):
-                xml = blob.group(1).encode("utf-8")
-                tree = etree.fromstring(xml)
-                text_nodes = tree.xpath(
-                    "//x:html//x:*/text()",  # Extract only HTML content, not GAAP junk
-                    namespaces={"x": "http://www.w3.org/1999/xhtml"},
-                )
-                for node in text_nodes:
-                    if len(node) > min_node_length:
-                        output.append(node + "\n")
+                html = blob.group(1)
+                output.append(html)
     return output
 
 
@@ -46,3 +36,4 @@ if __name__ == "__main__":
     dir = Path(default_dir)
     download_if_needed(dir)
     Path(output_file).open("w").writelines(clean_sgml_junk(dir))
+    print("Now run:\n cat source.html | w3m -dump -T text/html -cols 200 > source.txt")
