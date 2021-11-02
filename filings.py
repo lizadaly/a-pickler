9b516f2a4252bd2486e948b0dffffd11dccda8c8
Liza Daly <lizadaly@gmail.com>
2021-11-02 15:17:05 -0400

    Output

--- filings.py
+++ filings.py
@@ -4,6 +4,8 @@ from lxml import etree
 
 ticker_symbol = "TSLA"
 default_dir = "sec-edgar-filings"
+output_dir = "."
+output_file = "source.txt"
 
 
 def download_if_needed(dir: Path):
@@ -15,7 +17,11 @@ def download_if_needed(dir: Path):
         dl.get("10-Q", ticker_symbol)
 
 
-def clean_sgml_junk(dir: Path):
+def clean_sgml_junk(dir: Path) -> list[str]:
+    """Clean up an EDGAR filing by brute-force extracting all the text nodes of a reasonable length"""
+    output: list[str] = []
+    min_node_length = 5
+
     for filing_dir in dir.rglob("*"):
 
         filing = filing_dir / "full-submission.txt"
@@ -26,16 +32,17 @@ def clean_sgml_junk(dir: Path):
                 xml = blob.group(1).encode("utf-8")
                 tree = etree.fromstring(xml)
                 text_nodes = tree.xpath(
-                    "//x:html//text()",
+                    "//x:html//x:*/text()",  # Extract only HTML content, not GAAP junk
                     namespaces={"x": "http://www.w3.org/1999/xhtml"},
                 )
                 for node in text_nodes:
-                    if len(node) > 5:
-                        print(node)
+                    if len(node) > min_node_length:
+                        output.append(node + "\n")
+    return output
 
 
 if __name__ == "__main__":
 
     dir = Path(default_dir)
     download_if_needed(dir)
-    clean_sgml_junk(dir)
+    Path(output_file).open("w").writelines(clean_sgml_junk(dir))
